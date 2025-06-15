4c77cd9d559f02d41e772ad1d37c3077551a20e2
zlatapitsur <s31532@pjwstk.edu.pl>
2025-06-15 16:25:23 +0200

    push

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -1,57 +1,100 @@
 ﻿using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
+
 {
+
     public int itemIndex = 0;
 
     private bool playerInRange = false;
+
     private SimpleInventory inventory;
+
     private InventoryUI inventoryUI;
 
     void Update()
+
     {
+
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
+
         {
+
             if (inventory != null)
+
             {
+
                 Transform slot = inventory.GetSlot(itemIndex);
+
                 if (slot != null && slot.childCount == 0)
+
                 {
+
                     transform.position = slot.position;
+
                     transform.SetParent(slot);
+
                     inventory.UpdateUI();
+
                 }
 
                 if (inventoryUI != null)
+
                 {
+
                     inventoryUI.AddItem(itemIndex);
+
                     Destroy(gameObject); // видаляємо предмет зі сцени
+
                 }
 
                 Destroy(this); // відключаємо повторне додавання
+
             }
+
         }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
+
     {
+
         if (collision.CompareTag("Player"))
+
         {
+
             inventory = FindObjectOfType<SimpleInventory>();
+
             inventoryUI = FindObjectOfType<InventoryUI>();
+
             playerInRange = true;
+
         }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
+
     {
+
         if (collision.CompareTag("Player"))
+
         {
+
             playerInRange = false;
+
             inventory = null;
+
             inventoryUI = null;
+
         }
+
     }
+
 }
+
